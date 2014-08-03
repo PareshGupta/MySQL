fff15aa716c78f892c27d2b585fe94d9efba00a4
paresh <paresh@vinsol.com>
2014-08-03 22:09:13 +0530

    change backup file name

--- Backup/backup_restore.sql
+++ Backup/backup_restore.sql
@@ -1,9 +1,9 @@
 -- create backup for Multibranch_Library database
-mysqldump -u root -p Multibranch_Library > backup.sql
+mysqldump -u root -p Multibranch_Library > multibranch_library_dump.sql
 
 -- create restored database
 DROP DATABASE IF EXISTS restored;
 CREATE DATABASE restored;
 
 -- restore backup file to restored database
-mysql -u root -p restored < backup.sql
+mysql -u root -p restored < multibranch_library_dump.sql
