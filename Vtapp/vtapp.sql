26ecaaafc49d716ca0a76b9be741f74ada63156f
PareshGupta <paresh@vinsol.com>
2014-08-04 10:46:29 +0530

    give password to new user

--- Vtapp/vtapp.sql
+++ Vtapp/vtapp.sql
@@ -6,15 +6,15 @@ USE vtapp;
 
 -- create user 
 DROP USER 'vtapp_user'@'localhost';
-CREATE USER 'vtapp_user'@'localhost';
+CREATE USER 'vtapp_user'@'localhost' IDENTIFIED BY 'iamthebest';
 
 -- grant permissions to the user
 GRANT ALL ON vtapp.* TO 'vtapp_user'@'localhost';
 SHOW GRANTS FOR 'vtapp_user'@'localhost';
 
-+---------------------------------------------------------------+
-| Grants for vtapp_user@localhost                               |
-+---------------------------------------------------------------+
-| GRANT USAGE ON *.* TO 'vtapp_user'@'localhost'                |
-| GRANT ALL PRIVILEGES ON `vtapp`.* TO 'vtapp_user'@'localhost' |
-+---------------------------------------------------------------+
++-------------------------------------------------------------------------------------------------------------------+
+| Grants for vtapp_user@localhost                                                                                   |
++-------------------------------------------------------------------------------------------------------------------+
+| GRANT USAGE ON *.* TO 'vtapp_user'@'localhost' IDENTIFIED BY PASSWORD '*014CB9097FA3B66A517AA64D5F63ACA0171C6FD7' |
+| GRANT ALL PRIVILEGES ON `vtapp`.* TO 'vtapp_user'@'localhost'                                                     |
++-------------------------------------------------------------------------------------------------------------------+
