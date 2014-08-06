33d1f3dc4877f8aa4fe26e93ba54f791195d35f0
PareshGupta <paresh@vinsol.com>
2014-08-06 20:19:48 +0530

    add transaction exercise

---
+++ Transaction/transaction.sql
@@ -0,0 +1,118 @@
+-- create database bank
+DROP DATABASE IF EXISTS bank;
+CREATE DATABASE bank;
+USE bank;
+
+-- create table accounts
+DROP TABLE IF EXISTS accounts;
+
+CREATE TABLE accounts
+( id int AUTO_INCREMENT,
+  account_no bigint,
+  balance float,
+  PRIMARY KEY(id, account_no)
+);
+
+INSERT INTO accounts(account_no, balance) VALUES(119900480901, 25000);
+INSERT INTO accounts(account_no, balance) VALUES(119900480912, 15000);
+INSERT INTO accounts(account_no, balance) VALUES(119900480911, 15700);
+INSERT INTO accounts(account_no, balance) VALUES(119900480931, 10000);
+INSERT INTO accounts(account_no, balance) VALUES(119900480903, 50000);
+
++----+--------------+---------+
+| id | account_no   | balance |
++----+--------------+---------+
+|  1 | 119900480901 |   25000 |
+|  2 | 119900480912 |   15000 |
+|  3 | 119900480911 |   15700 |
+|  4 | 119900480931 |   10000 |
+|  5 | 119900480903 |   50000 |
++----+--------------+---------+
+
+-- create table users
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+( id int AUTO_INCREMENT,
+  name varchar(255),
+  email varchar(255),
+  account_no_id int,
+  PRIMARY KEY(id),
+  FOREIGN KEY(account_no_id) REFERENCES accounts(id)
+);
+
+INSERT INTO users(name, email, account_no_id) VALUES('Paresh', 'paresh@vinsol.com', 1);
+INSERT INTO users(name, email, account_no_id) VALUES('Ankur', 'ankur@vinsol.com', 2);
+INSERT INTO users(name, email, account_no_id) VALUES('Bipin', 'bipin@vinsol.com', 3);
+INSERT INTO users(name, email, account_no_id) VALUES('Tanny', 'tanny@vinsol.com', 4);
+INSERT INTO users(name, email, account_no_id) VALUES('Deva', 'deva@vinsol.com', 5);
+
++----+--------+-------------------+---------------+
+| id | name   | email             | account_no_id |
++----+--------+-------------------+---------------+
+|  1 | Paresh | paresh@vinsol.com |             1 |
+|  2 | Ankur  | ankur@vinsol.com  |             2 |
+|  3 | Bipin  | bipin@vinsol.com  |             3 |
+|  4 | Tanny  | tanny@vinsol.com  |             4 |
+|  5 | Deva   | deva@vinsol.com   |             5 |
++----+--------+-------------------+---------------+
+
+-- userA is depositing Rs 1000/- his account
+BEGIN;
+SET autocommit = 0;
+UPDATE accounts INNER JOIN users ON accounts.id = users.account_no_id
+SET accounts.balance = accounts.balance + 1000
+WHERE users.name = 'Paresh';
+COMMIT;
+
++----+--------------+---------+
+| id | account_no   | balance |
++----+--------------+---------+
+|  1 | 119900480901 |   26000 |
+|  2 | 119900480912 |   15000 |
+|  3 | 119900480911 |   15700 |
+|  4 | 119900480931 |   10000 |
+|  5 | 119900480903 |   50000 |
++----+--------------+---------+
+
+-- userA is withdrawing Rs 500/-
+BEGIN;
+SET autocommit = 0;
+UPDATE accounts INNER JOIN users ON accounts.id = users.account_no_id
+SET accounts.balance = accounts.balance - 500
+WHERE users.name = 'Tanny';
+COMMIT;
+
++----+--------------+---------+
+| id | account_no   | balance |
++----+--------------+---------+
+|  1 | 119900480901 |   26000 |
+|  2 | 119900480912 |   15000 |
+|  3 | 119900480911 |   15700 |
+|  4 | 119900480931 |    9500 |
+|  5 | 119900480903 |   50000 |
++----+--------------+---------+
+
+-- userA is transferring Rs 200/- to userB's account
+BEGIN;
+SET autocommit = 0;
+UPDATE accounts INNER JOIN users
+ON accounts.id = users.account_no_id 
+SET accounts.balance = accounts.balance - 200
+WHERE users.name = 'Bipin';
+
+UPDATE accounts INNER JOIN users
+ON accounts.id = users.account_no_id
+SET accounts.balance = accounts.balance + 200
+WHERE users.name = 'Deva';
+COMMIT;
+
++----+--------------+---------+
+| id | account_no   | balance |
++----+--------------+---------+
+|  1 | 119900480901 |   26000 |
+|  2 | 119900480912 |   15000 |
+|  3 | 119900480911 |   15500 |
+|  4 | 119900480931 |    9500 |
+|  5 | 119900480903 |   50200 |
++----+--------------+---------+
---
+++ Transaction/transaction_dump.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: bank
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_no` bigint(20) NOT NULL DEFAULT '0',
+  `balance` float DEFAULT NULL,
+  PRIMARY KEY (`id`,`account_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,119900480901,26000),(2,119900480912,15000),(3,119900480911,15500),(4,119900480931,9500),(5,119900480903,50200);
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `account_no_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_no_id` (`account_no_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`account_no_id`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Paresh','paresh@vinsol.com',1),(2,'Ankur','ankur@vinsol.com',2),(3,'Bipin','bipin@vinsol.com',3),(4,'Tanny','tanny@vinsol.com',4),(5,'Deva','deva@vinsol.com',5);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-06 20:16:53
